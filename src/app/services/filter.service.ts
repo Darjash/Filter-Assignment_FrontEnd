0fe50367fab77a6696623b9c6e769258ac65646d
Darjash <darja.skiljova@gmail.com>
2024-09-15 18:43:21 +0300

    "first commit"

---
+++ src/app/services/filter.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Filter } from '../filter.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilterService {
+  private baseUrl = 'http://localhost:8080/filters';
+  constructor(private http: HttpClient) { }
+
+  getAllFilters(): Observable<Filter[]> {
+    return this.http.get<Filter[]>(this.baseUrl);
+  }
+
+  createFilter(filter: Filter): Observable<Filter> {
+    return this.http.post<Filter>(`${this.baseUrl}/add`, filter);
+  }
+}
